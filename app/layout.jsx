1c9bddb7b61402db6afacd5772b71af31a86b40d
Dmytro-Skaliun <dskaliun@gmail.com>
2024-12-02 20:51:23 +0100

    Added initial components, connected to Appwriter io

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,9 @@
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
 
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,9 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <main className="m-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
--- app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1>Nextjs Booking App</h1>
-    </>
-  );
-}
